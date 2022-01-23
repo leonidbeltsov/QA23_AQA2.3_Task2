089c4b8df5c659193eb06fb576f7cc4d4926b96a
leonidbeltsov <leonidbeltsov@gmail.com>
2022-01-23 21:58:14 +0300

    Teacher's Comment

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -1,15 +1,40 @@
 package ru.netology.data;
 
 import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
 import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
 
 public class DataGenerator {
     private DataGenerator() {
     }
 
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
     public static class RegistrationInfo {
         private RegistrationInfo() {
+        }
+
+        public static void setUpUser(UserInfo user) {
+            // сам запрос
+            given() // "дано"
+                    .spec(requestSpec) // указываем, какую спецификацию используем
+                    .body(user)
+                    .when() // "когда"
+                    .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                    .then() // "тогда ожидаем"
+                    .statusCode(200); // код 200 OK
         }
 
         public static String makeUserPassword(String Locale) {
@@ -22,11 +47,15 @@ public class DataGenerator {
             return faker.name().username();
         }
 
-        public static UserInfo generateUserInfo(String Locale, boolean userStatusIsActive) {
+        public static String setUserStatus(String status) {
+            return status;
+        }
+
+        public static UserInfo generateUserInfo(String Locale, String userStatus) {
             return new UserInfo(
                     makeUserName(Locale),
                     makeUserPassword(Locale),
-                    (userStatusIsActive) ? "active" : "blocked"
+                    setUserStatus(userStatus)
             );
         }
     }
--- src/test/java/ru/netology/test/ApiLoginTest.java
+++ src/test/java/ru/netology/test/ApiLoginTest.java
@@ -2,10 +2,6 @@ package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataGenerator;
@@ -17,32 +13,13 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static io.restassured.RestAssured.given;
+import static ru.netology.data.DataGenerator.RegistrationInfo.setUpUser;
 
 public class ApiLoginTest {
 
     @BeforeEach
     void headlessMode() {
         Configuration.headless = true;
-    }
-
-    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
-
-    private void setUpUser(UserInfo user) {
-        // сам запрос
-        given() // "дано"
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(user)
-                .when() // "когда"
-                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-                .then() // "тогда ожидаем"
-                .statusCode(200); // код 200 OK
     }
 
     private void loginForm(String login, String password) {
@@ -54,7 +31,7 @@ public class ApiLoginTest {
 
     @Test
     public void shouldLoginExistsActiveUser() {
-        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", true);
+        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", "active");
         setUpUser(user);
         loginForm(user.getLogin(), user.getPassword());
         $(byText("Личный кабинет")).shouldBe(Condition.visible, Duration.ofSeconds(15));
@@ -62,7 +39,7 @@ public class ApiLoginTest {
 
     @Test
     public void shouldNotLoginExistsBlockedUser() {
-        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", false);
+        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", "blocked");
         setUpUser(user);
         loginForm(user.getLogin(), user.getPassword());
 
@@ -71,7 +48,7 @@ public class ApiLoginTest {
 
     @Test
     public void shouldNotLoginWithWrongUsername() {
-        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", true);
+        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", "active");
         setUpUser(user);
         loginForm(DataGenerator.RegistrationInfo.makeUserName("ru"), user.getPassword());
         $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible, Duration.ofSeconds(15));
@@ -79,7 +56,7 @@ public class ApiLoginTest {
 
     @Test
     public void shouldNotLoginWithWrongPassword() {
-        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", true);
+        UserInfo user = DataGenerator.RegistrationInfo.generateUserInfo("ru", "active");
         setUpUser(user);
         loginForm(user.getLogin(), DataGenerator.RegistrationInfo.makeUserPassword("ru"));
         $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible, Duration.ofSeconds(15));
